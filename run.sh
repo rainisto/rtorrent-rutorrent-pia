b809c370900d572f94727514f253f7471b48f4c0
Jonni Rainisto <jonni.rainisto@gmail.com>
2025-04-23 22:48:37 +0300

    Optimise port updating

--- run.sh
+++ run.sh
@@ -6,32 +6,3 @@ docker run --rm -it httpd:2.4-alpine htpasswd -Bbn ${RU_USERNAME:-admin} ${RU_PA
 docker run --rm -it httpd:2.4-alpine htpasswd -Bbn ${RU_USERNAME:-admin} ${RU_PASSWORD:-salasana} > ./passwd/rutorrent.htpasswd
 
 docker-compose up -d
-
-# Install inotify-tools on host (if needed)
-if ! command -v inotifywait >/dev/null; then
-  echo "Installing inotify-tools..."
-  sudo apt-get update && sudo apt-get install -y inotify-tools || \
-  sudo yum install -y inotify-tools || \
-  sudo apk add --no-cache inotify-tools
-fi
-
-while true; do
-  if [ ! -f "./gluetun/forwarded_port" ]; then
-    echo "Waiting for forwarded_port file..."
-    sleep 2
-  else
-     break
-  fi
-done
-
-# run once at start
-docker exec -it rtorrent-rutorrent /scripts/update-port.sh
-# run again if port changes
-while true; do
-  inotifywait -q -e modify ./gluetun/forwarded_port
-  echo "[$(date +'%Y-%m-%d %H:%M:%S')] Port changed - updating rTorrent"
-  docker exec -it rtorrent-rutorrent /scripts/update-port.sh
-done
-
-
-
